28ec3266d05903b50b138cfc1356e313c37b7b22
Duan <duanpretorius15@gmail.com>
2022-10-13 15:03:01 -0700

    puzzle solved

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts - Prints text in quotes 
+ */
+int main(void)
+{
+	puts("Programming is like a multilingual puzzle\n");
+
+	return (0);
+}
