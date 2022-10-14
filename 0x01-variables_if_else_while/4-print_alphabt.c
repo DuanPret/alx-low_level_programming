66fe224d4f9702560c10506e40d878327254562d
Duan <duanpretorius15@gmail.com>
2022-10-14 16:02:06 -0700

    single digit base 10 num

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* Prints alphabet in lowercase without q & e */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+}
