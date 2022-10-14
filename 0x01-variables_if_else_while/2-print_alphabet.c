a3037b1575d6a9b096984108ffa5dbc16cb462b6
Duan <duanpretorius15@gmail.com>
2022-10-14 14:44:06 -0700

    a to zzzzz

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* prints alphabet in lower case letters */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
