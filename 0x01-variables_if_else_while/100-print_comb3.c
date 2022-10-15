9e9d24999fb1d7b68f1263db7c81f15282227273
Duan <duanpretorius15@gmail.com>
2022-10-14 19:26:58 -0700

    2 digit comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/* printing all combinations of given numbers */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0  (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			if (a != b)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				if (a + b < 17)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
