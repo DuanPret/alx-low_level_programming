73518f6a81ad1f50b26d0bd06c4ff286f0e112d0
Duan <duanpretorius15@gmail.com>
2022-10-18 18:35:30 -0700

    1+1=EZ

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers together
+ * @x: first number
+ * @y: second number
+ *
+ * Return: sum of the two numbers
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
