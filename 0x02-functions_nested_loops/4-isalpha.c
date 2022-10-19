bd91cf8e022512f499fe7c97fd88a13ed1adad8b
Duan <duanpretorius15@gmail.com>
2022-10-18 17:07:37 -0700

    alpha or no

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if input is is a character	is from English alphabet
+ *
+ * @c: input parameter
+ *
+ * Return: 1 if true, 0 if false
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
