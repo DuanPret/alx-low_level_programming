257bb0f72348365a01028b25669ff928aee422af
Duan <duanpretorius15@gmail.com>
2022-10-20 06:00:34 -0700

    ___

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ *
+ * @n: number of lines to print
+ * Return: void
+ */
+void print_line(int n)
+{
+	int p;
+
+	for (p = 0; p <= n; p++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
