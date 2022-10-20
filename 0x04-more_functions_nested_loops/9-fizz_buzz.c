62a639e933bab2c85e99823b2851d8bbe406464d
Duan <duanpretorius15@gmail.com>
2022-10-20 09:03:32 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	while (i <= 100)
 	{
-		if (((i % 3) == 0) && ((i % 5) == 0))
+		if (i % (3 * 5) == 0)
 		{
-		printf("FizzBuzz ");
+			printf("FizzBuzz ");
 		}
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
@@ -29,6 +29,7 @@ int main(void)
 		{
 			printf("%d ", i);
 		}
+		i++;
 	}
 	putchar('\n');
 	return (0);
