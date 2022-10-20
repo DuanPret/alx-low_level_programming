1a41271f34320b3e3f94fa4a7766464b5a7f3589
Duan <duanpretorius15@gmail.com>
2022-10-20 09:09:31 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
  * main - counts from one to hundred and every multiple of three prints Fizz
  * and every multiple of 5 prints Buzz, if multiple of both print FizzBuzz
  *
- * Return: Always 0
+ * Return: Result
  */
 int main(void)
 {
@@ -14,23 +14,20 @@ int main(void)
 	while (i <= 100)
 	{
 		if (i % (3 * 5) == 0)
-		{
 			printf("FizzBuzz ");
-		}
+		
 		else if (i % 3 == 0)
-		{
 			printf("Fizz ");
-		}
+		
 		else if (i % 5 == 0)
-		{
 			printf("Buzz ");
-		}
+		
 		else
-		{
 			printf("%d ", i);
-		}
+		
 		i++;
 	}
 	putchar('\n');
 	return (0);
+	
 }
