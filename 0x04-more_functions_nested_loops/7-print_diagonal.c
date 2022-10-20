3be6bcb2dec86581b8bb43519b8d2444456cc04d
Duan <duanpretorius15@gmail.com>
2022-10-20 08:52:47 -0700

    fix for 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,25 +3,23 @@
 /**
  * print_diagonal - prints a diagonal / followed by new line
  *
- * @n: number of spaces to print before /
+ * @n: input variable
  * Return: void
  */
 void print_diagonal(int n)
 {
 	int p, q;
 
-	for (p = 0; p <= n; p++)
-	{
-		for (q = 0; q <= n; q++)
-		{
-			if (q == p && n > 0)
-			{
-				_putchar('\\');
-				_putchar('\n');
-			}
-			else
-				_putchar(' ');
-		}
-	}
+	if (n <= 0)
 		_putchar('\n');
+
+	for (p = 0; p < n; p++)
+	{
+		for (q = 0; q < p; q++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
