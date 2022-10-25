7e6f4b786a66f445d634a9da3196283c14bf3506
Duan <duanpretorius15@gmail.com>
2022-10-24 19:20:13 -0700

    enod

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_rev - reverse string
+ *
+ * @s: string to reverse
+ * Return: nothing.
+ */
+void rev_string(char *s)
+{
+	int i, r;
+	char *b, *e, ch;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+		r = i;
+
+	b = s;
+	e = s;
+
+	for (i = 0; i < r; i++)
+		e++;
+
+	for (i = 0; i < r/2; i++)
+	{
+		ch = *e;
+
+		*e = *b;
+		*b = ch;
+
+		b++;
+		e--;
+	}
+}
