13265c052b195bca6b3184d25542e662042fd9d2
Duan <duanpretorius15@gmail.com>
2022-10-24 17:20:46 -0700

    don't think this is right but here

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - calculates the size of a string
+ *
+ * @s: string that code is checking
+ * Return: always 0.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+
+	return (i);
+}
