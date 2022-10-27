65cef1c6c3a3f88446395abd1c2eefcaa6ca59e8
Duan <duanpretorius15@gmail.com>
2022-10-27 03:03:38 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: 
+ * @src:
+ * @n:
+ * Return: dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j = 0;
+
+	for (i = 0; i < 1000; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		j++;
+	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
+
+	return (dest);
+}
