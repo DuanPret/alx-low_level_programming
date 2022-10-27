9c4bc70f355efc4e6ae258bde0b254b50c41ad21
Duan <duanpretorius15@gmail.com>
2022-10-27 12:38:12 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words in given string
+ *
+ * @s: string input
+ * Return: s.
+ */
+char *cap_string(char *s)
+{
+	int i, d;
+	char delimeters[] = "\t\n,;.!?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+
+		for (d = 0; delimeters[d] != '\0'; d++)
+			if (s[i] == delimeters[d] && s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+	}
+	return (s);
+}
