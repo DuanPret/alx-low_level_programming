b3d666046cda29ecb5f3e14fd30109ba7ce8b373
Duan <duanpretorius15@gmail.com>
2022-11-01 18:29:44 -0700

    fix

--- 0x07-pointers_arrays_strings/8-print_diagsum.c
+++ 0x07-pointers_arrays_strings/8-print_diagsum.c
@@ -11,11 +11,11 @@ void print_diagsum(int *a, int size)
 	int i, c = 0, d = 0;
 
 	for (i = 0; i < size; i++)
-		{
-			c += a[i];
-			d += a[size - i - 1];
-			a += size;
-		}
+	{
+		c += a[i];
+		d += a[size - i - 1];
+		a += size;
+	}
 
 	printf("%d", c);
 	printf("%d\n", d);
