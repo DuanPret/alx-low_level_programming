affb32009f91a3c06c0544b374e1a3fc9909b27b
Duan <duanpretorius15@gmail.com>
2022-11-01 18:21:03 -0700

    print sum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsum - prints the sum of two diagonals
+ * @a: pointer to array
+ * @size: Size of matrix
+ * Return: nothing.
+ */
+void print_diagsum(int *a, int size)
+{
+	int i, c = 0, d = 0;
+
+	for (i = 0; i < size; i++)
+		{
+			c += a[i];
+			d += a[size - i - 1];
+			a += size;
+		}
+
+	printf("%d", c);
+	printf("%d\n", d);
+}
