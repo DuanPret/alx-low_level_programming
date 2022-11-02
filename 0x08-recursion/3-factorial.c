70db7b2a0090159bab6e4f5607d8fb9d8422be17
Duan <duanpretorius15@gmail.com>
2022-11-02 05:30:37 -0700

    fixed factorial for 0

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,12 +3,14 @@
  * factorial - factorial of given input
  *
  * @n: integer input
- * Return: if lower than zero return -1 else return factorial
+ * Return: result of @n factorial
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
+	else if (n == 0)
+		return(1);
 
 	return (n * factorial(n - 1));
 }
