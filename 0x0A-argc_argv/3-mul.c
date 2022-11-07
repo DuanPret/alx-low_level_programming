c4ba60aa534b500b792f388d7d4ad88cc9a9b7cd
Duan <duanpretorius15@gmail.com>
2022-11-07 07:00:34 -0800

    typo fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - multiplies two given integers toghether
+ * main - multiplies two numbers
  *
  * @argc: length of @argv
  * @argv: array of strings of program's arguments
- * Retun: 0 on success, on error return 1
+ * Return: 0 on success, on error return 1
  */
 int main(int argc, char *argv[])
 {
