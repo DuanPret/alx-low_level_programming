a93b9df6d32e362096c79f69185aad965edc6d40
Duan <duanpretorius15@gmail.com>
2022-11-07 04:31:32 -0800

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ *
+ * @argc: length of argv
+ * @argv: arrays of strings of program's arguments
+ * Return: 0 on success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	
+	return (0);
+}
