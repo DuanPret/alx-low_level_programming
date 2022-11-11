12cce9c90372a558277e6f5ec4de2927887c1fdf
Duan <duanpretorius15@gmail.com>
2022-11-11 15:09:06 -0800

    multiply with malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - multiply two numbers as arguments
+ *
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv [])
+{
+	unsigned long mult;
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mult = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mult);
+	return (0);
+}
