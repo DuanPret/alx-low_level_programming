ca960f27cc01b2c36e34acd74002b041e4f25d02
Duan <duanpretorius15@gmail.com>
2022-11-17 02:20:38 -0800

    fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 			exit(2);
 		}
 		for (i = 0; i < n_bytes; i++)
-			printf("%02x%c", 
+			printf("%02x%c",
 					(unsigned char)*((char *)prog + i), i < n_bytes - 1 ? ' ' : '\n');
 	}
 	else
