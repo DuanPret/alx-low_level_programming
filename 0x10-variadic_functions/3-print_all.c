6d000c41287e8ff91d36b9bd0a075dcc71cfc403
Duan <duanpretorius15@gmail.com>
2022-11-18 03:26:56 -0800

    fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,8 +1,8 @@
-#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 
-void (*get_func(char identifier struct format_struct *fmt_arr))(va_list *);
+void (*get_func(char identifier, struct format_struct *fmt_arr))(va_list *);
 void print_char(va_list *arg);
 void print_int(va_list *arg);
 void print_float(va_list *arg);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,24 @@
 #ifndef VARDIADIC_FUNCTIONS_H
 #define VARDIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct format_struct - Format-type class
+ *
+ * @format: Format identifier
+ * @fmt_print_func: Pointer to function that prints in the format of @format
+ */
+struct format_struct
+{
+	char format;
+	void (*fmt_print_func)(va_list *arg_list);
+};
+
+typedef struct format_struct format_struct_ptr;
 
 #endif
