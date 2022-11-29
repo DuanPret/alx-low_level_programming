aacb8e983f324889a3fd071d284ce45aa11d4816
Duan <duanpretorius15@gmail.com>
2022-11-29 02:32:06 -0800

    print list

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef SINGLY_LINKED_LIST_H
+#define SIGLY_LINKED_LIST_H
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
