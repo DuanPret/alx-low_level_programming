06499f2c8b0e0e9d375a53ea3d6a701e80c59133
Duan <duanpretorius15@gmail.com>
2022-11-29 04:59:58 -0800

    adding new nodes to the top

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - Adds a new node at the beginning of the list
+ * @n: Number of nodes
+ * #head: First node of list
+ * Return: The address of the first new element, NULL on error
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_head = malloc(sizeof(listint_t));
+
+	if (new_head == NULL)
+		return (NULL);
+
+	new_head->n = n;
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
+}
