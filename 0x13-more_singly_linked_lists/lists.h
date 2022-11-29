bb21e3780782315fa6b44e864e3710c03688b18c
Duan <duanpretorius15@gmail.com>
2022-11-29 03:35:39 -0800

    fix?

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
-#ifndef SINGLY_LINKED_LIST_H
-#define SIGLY_LINKED_LIST_H
+#ifndef MORE_SINGLY_LINKED_LIST_H
+#define MORE_SINGLY_LINKED_LIST_H
 
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdef.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
