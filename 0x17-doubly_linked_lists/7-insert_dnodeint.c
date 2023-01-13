b4014323b1b142aea4dcfef3a30f841d9ece6118
Duan <duanpretorius15@gmail.com>
2023-01-12 16:21:18 -0800

    fixed giving incorrect return

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			(*h)->prev = new;
 		(*h) = new;
 		new->prev = NULL;
-		return (NULL);
+		return (new);
 	}
 
 	tmp = *h;
