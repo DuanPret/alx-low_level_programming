b485f7db7557a50804f2177ba2c0a15e45a549c8
Duan <duanpretorius15@gmail.com>
2023-01-12 16:17:25 -0800

    fixed?

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tmp = tmp->next;
 	}
 
-	if (!tmp)
+	if (tmp == NULL)
 		return (NULL);
 	new->next = tmp->next;
 	new->prev = tmp;
